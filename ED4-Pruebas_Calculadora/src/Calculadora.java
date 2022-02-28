3042acedbf2bc7cac98b80dc8dd53bbba1dc811a
Lionel <lionel.ta@gmail.com>
2022-02-28 13:52:58 +0100

    Añadido proyecto ED4-Pruebas & Ejemplo CalculadoraTest

---
+++ ED4-Pruebas_Calculadora/src/Calculadora.java
@@ -0,0 +1,30 @@
+public class Calculadora {
+
+    private int num1;
+    private int num2;
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int suma() {
+        int result = num1 + num2;
+        return result;
+    }
+
+    public int resta() {
+        int result = num1 - num2;
+        return result;
+    }
+
+    public int multiplica() {
+        int result = num1 * num2;
+        return result;
+    }
+
+    public int divide() {
+        int result = num1 / num2;
+        return result;
+    }
+}
