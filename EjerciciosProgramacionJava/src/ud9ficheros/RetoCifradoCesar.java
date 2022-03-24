2c3a08750bfc33e7d4c058379c871c00d3cdafb1
lionel-ict <lionel.ta@gmail.com>
2022-03-24 20:59:34 +0100

    Añadido Reto Cifrado Cesar (por terminar)

---
+++ EjerciciosProgramacionJava/src/ud9ficheros/RetoCifradoCesar.java
@@ -0,0 +1,32 @@
+package ud9ficheros;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RetoCifradoCesar {
+    
+    public static void main(String[] args) {
+        
+        String texto = "";
+        
+        try {
+            
+            File f = new File("src/ud9ficheros/carta-cifrada.txt");
+            Scanner sc = new Scanner(f);
+            texto = sc.nextLine();
+            
+            //TODO TERMINAR
+            
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        
+        System.out.println(texto);
+        
+                
+        
+        
+    }
+    
+}
